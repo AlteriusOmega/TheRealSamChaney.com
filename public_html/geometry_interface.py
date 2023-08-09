be46d538a4c7300ee235409b6b89ced2ae7d734b
Alterius <therealsamchaney@gmail.com>
2023-08-09 15:37:08 -0500

    Got PyScript working completely including getting repl to access code from the geometry_interface.py. Also created Javascript function getPolygonSize to get the size of all the polygons so we can dynamically adjust the SVG tag size. Was able to import that into PyScript block using from js import getPolygonSize. Now need to implement function that takes those polygon sizes and calculates proper size of SVG element.

--- public_html/geometry_interface.py
+++ public_html/geometry_interface.py
@@ -2,34 +2,31 @@ from geometry_generator import *
 import js
 import svgwrite
 
-# await micropip.install("svgwrite")
+print(f"geometry_interface.py ran!")
 
 interface_element = Element("interface")
 
-print("print test!")
+svg_output_width = 500
+diameter = svg_output_width / 2
 
-diameter = 100
-
-center = [diameter, diameter]
-
-triangle = Polygon(3, diameter, center)
+center = [svg_output_width*0.5, svg_output_width*0.5]
+print(f"center is {center}")
 
 svg_output = Element("svg-output")
 
-triangle.draw()
+# Note, drawing_global is global variable from geometry_generator.py itself 
 
-svg_string = drawing_global.tostring()
-
-# print(svg_string)
-
-svg_output.element.innerHTML = svg_string
-
-def test_function():
-    print("test function ran!")
-    
 repl = Element("py-repl")
-    
-def run_geometry():
+
+def render_drawing_html():
+    print(f"render_drawing_html ran!")
+    print(f"printed right after printing render_drawing_html ran!")
+    svg_string = drawing_global.tostring()
+    print(f"svg_string is {svg_string}")
+    svg_output.element.innerHTML = svg_string
+
+def clear_drawing():
+    global drawing_global
+    print(f"clear_drawing ran! and drawing is {drawing_global} and drawing xml is {drawing_global.get_xml()}")
     drawing_global = svgwrite.Drawing(output_path)
-    print(f"repl contents was {repl.element.getPySrc()}")
-    
+    render_drawing_html() # Render the drawing to update the page with empty drawing
